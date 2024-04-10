e4729680c6a358a5382a428b7b61c9a988291831
Felipe Goulart <luisfelipegoulart94@gmail.com>
2024-04-10 18:22:06 -0300

    Where is the batman?

---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-header class="app-navbar">
+    <q-toolbar class="app-navbar__toolbar">
+      <AppLogo />
+
+      <AppDesktopMenu />
+    </q-toolbar>
+  </q-header>
+</template>
+
+<script setup>
+import AppLogo from './AppLogo.vue'
+
+</script>
+
+<style lang="scss">
+.app-navbar {
+  background-color: transparent !important;
+  padding: 20px 28px;
+
+  &__toolbar {
+    display: flex;
+    max-width: 1024px;
+    width: 100%;
+    margin: 0 auto
+  }
+}
+</style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <AppNavbar />
+
+    <!-- <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header>
+          Essential Links
+        </q-item-label>
+
+        <EssentialLink v-for="link in linksList" :key="link.title" v-bind="link" />
+      </q-list>
+    </q-drawer> -->
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import AppNavbar from 'src/components/AppNavbar.vue'
+
+defineOptions({
+  name: 'MainLayout'
+})
+</script>
